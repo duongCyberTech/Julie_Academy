03106ea86c8c3ea1f157112aee9f436ff4246a78
dCyberTech <maidangduong92tn@gmail.com>
2026-01-24 18:02:15 +0700

    Map Exam Taken to Question

--- tutor-service/src/exam/exam_taken.service.ts
+++ tutor-service/src/exam/exam_taken.service.ts
@@ -15,122 +15,147 @@ export class ExamTakenService {
     ){}
 
     async takeExam(class_id: string, exam_id: string, session_id: number, student_id: string) {
-        const isStudentInClass = await this.prisma.learning.findUnique({where: {class_id_student_uid: {class_id, student_uid: student_id}}});
-        if (!isStudentInClass) {
-            throw new BadRequestException("Student is not in class")
-        }
-
-        const isExamOpen = this.prisma.exam_open_in.findUnique({where: {session_id_exam_id_class_id: {session_id, exam_id, class_id}}})
-        if (!isExamOpen) {
-            throw new BadRequestException("Exam is not open in this class")
-        }
-
-        const now = DateTime.now().setZone('Asia/Ho_Chi_Minh').toJSDate()
-        const isTimeValid = await this.prisma.exam_session.findUnique({
-            where: {
-                exam_id_session_id: {exam_id, session_id},
-                startAt: {lte: now},
-                expireAt: {gt: now}
+        return await this.prisma.$transaction(async(tx) => {
+            const isStudentInClass = await this.prisma.learning.findUnique({where: {class_id_student_uid: {class_id, student_uid: student_id}}});
+            if (!isStudentInClass) {
+                throw new BadRequestException("Student is not in class")
             }
-        })
 
-        if (!isTimeValid) {
-            throw new BadRequestException("Exam is not available now")
-        }
-
-        const timeTaken = await this.prisma.exam_taken.count({
-            where: {exam_id, session_id}
-        })
-
-        const takenLimit = await this.prisma.exam_session.findUnique({
-            where: {exam_id_session_id: {exam_id, session_id}},
-            select: {
-                limit_taken: true
+            const isExamOpen = this.prisma.exam_open_in.findUnique({where: {session_id_exam_id_class_id: {session_id, exam_id, class_id}}})
+            if (!isExamOpen) {
+                throw new BadRequestException("Exam is not open in this class")
             }
-        })
 
-        if (timeTaken >= takenLimit.limit_taken) {
-            throw new BadRequestException("No more time available")
-        }
-
-        const testInfo = await this.prisma.exam_session.findUnique({
-            where: {
-                exam_id_session_id: {exam_id, session_id}
-            },
-            select: {
-                exam: {
-                    select: {
-                        title: true,
-                        description: true,
-                        total_ques:true,
-                        level: true,
-                        duration: true,
-                    }
-                },
-                exam_id: true,
-                session_id: true,
-                startAt: true,
-                expireAt: true,
-                limit_taken: true,
-                exam_type: true
-            }
-        }).then((info) => {
-            return {
-                exam_id: info.exam_id,
-                session_id: info.session_id,
-                title: info.exam.title,
-                description: info.exam.description,
-                level: info.exam.level,
-                duration: info.exam.duration,
-                startAt: info.startAt,
-                expireAt: info.expireAt,
-                limit_taken: info.limit_taken,
-                exam_type: info.exam_type
-            }
-        })
-
-        const questionList = await this.prisma.questions.findMany({
-            where: {
-                exam_questions: {some: {exam: {exam_id}}}
-            },
-            select: {
-                ques_id: true,
-                title: true,
-                content: true,
-                explaination: true,
-                type: true,
-                level: true,
-                answers: {
-                    select: {
-                        aid: true,
-                        content: true,
-                        explaination: true,
-                        is_correct: true
-                    }
-                }
-            }
-        }).then((questions) => {
-            return questions.map((ques) => {
-                return {
-                    ...ques,
-                    answers: ques.answers.map((ans) => {
-                        return {
-                            ...ans,
-                            ...(testInfo.exam_type == ExamType.practice ? {
-                                explaination: ans.explaination,
-                                is_correct: ans.is_correct
-                            } : {})
-                        }
-                    })
+            const now = DateTime.now().setZone('Asia/Ho_Chi_Minh').toJSDate()
+            const isTimeValid = await this.prisma.exam_session.findUnique({
+                where: {
+                    exam_id_session_id: {exam_id, session_id},
+                    startAt: {lte: now},
+                    expireAt: {gt: now}
                 }
             })
-        });
 
-        return {
-            message: "Taking exam",
-            info: testInfo,
-            questions: questionList
-        }
+            if (!isTimeValid) {
+                throw new BadRequestException("Exam is not available now")
+            }
+
+            const timeTaken = await this.prisma.exam_taken.count({
+                where: {exam_id, session_id}
+            })
+
+            const takenLimit = await this.prisma.exam_session.findUnique({
+                where: {exam_id_session_id: {exam_id, session_id}},
+                select: {
+                    limit_taken: true
+                }
+            })
+
+            if (timeTaken >= takenLimit.limit_taken) {
+                throw new BadRequestException("No more time available")
+            }
+
+            const testInfo = await this.prisma.exam_session.findUnique({
+                where: {
+                    exam_id_session_id: {exam_id, session_id}
+                },
+                select: {
+                    exam: {
+                        select: {
+                            title: true,
+                            description: true,
+                            total_ques:true,
+                            level: true,
+                            duration: true,
+                        }
+                    },
+                    exam_id: true,
+                    session_id: true,
+                    startAt: true,
+                    expireAt: true,
+                    limit_taken: true,
+                    exam_type: true
+                }
+            }).then((info) => {
+                return {
+                    exam_id: info.exam_id,
+                    session_id: info.session_id,
+                    title: info.exam.title,
+                    description: info.exam.description,
+                    level: info.exam.level,
+                    duration: info.exam.duration,
+                    startAt: info.startAt,
+                    expireAt: info.expireAt,
+                    limit_taken: info.limit_taken,
+                    exam_type: info.exam_type
+                }
+            })
+
+            const questionList = await this.prisma.questions.findMany({
+                where: {
+                    exam_questions: {some: {exam: {exam_id}}}
+                },
+                select: {
+                    ques_id: true,
+                    title: true,
+                    content: true,
+                    explaination: true,
+                    type: true,
+                    level: true,
+                    answers: {
+                        select: {
+                            aid: true,
+                            content: true,
+                            explaination: true,
+                            is_correct: true
+                        }
+                    }
+                }
+            }).then((questions) => {
+                return questions.map((ques, index) => {
+                    return {
+                        ...ques,
+                        index,
+                        answers: ques.answers.map((ans) => {
+                            return {
+                                ...ans,
+                                ...(testInfo.exam_type == ExamType.practice ? {
+                                    explaination: ans.explaination,
+                                    is_correct: ans.is_correct
+                                } : {})
+                            }
+                        })
+                    }
+                }).sort((a, b) => a.index - b.index)
+            });
+
+            const takenTime = await tx.exam_taken.create({
+                data: {
+                    startAt: now,
+                    doneAt: now,
+                    final_score: 0,
+                    total_ques_completed: 0,
+                    exam_id,
+                    session_id,
+                    student: {connect: {uid: student_id}}
+                }
+            })
+
+            await tx.question_for_exam_taken.createMany({
+                data: questionList.map((item) => {
+                    return {
+                        et_id: takenTime.et_id,
+                        ques_id: item.ques_id,
+                        index: item.index
+                    }
+                })
+            })
+
+            return {
+                message: "Taking exam",
+                info: {et_id: takenTime.et_id, ...testInfo},
+                questions: questionList
+            }
+        })
     }
 
     async calculateScore(tx: Prisma.TransactionClient, answers: SubmitAnswerDto[]) {
@@ -185,30 +210,50 @@ export class ExamTakenService {
         return {score, cnt}
     }
 
-    async submitExam(class_id: string, exam_id: string, session_id: number, student_id: string, answers: SubmitAnswerDto[], startAt: Date, endAt: Date) {
+    async submitExam(class_id: string, et_id: string, isDone: boolean, student_id: string, answers: SubmitAnswerDto[]) {
         return await this.prisma.$transaction(async (tx) => {
             const isStudentInClass = await tx.learning.findUnique({where: {class_id_student_uid: {class_id, student_uid: student_id}}});
             if (!isStudentInClass) {
                 throw new BadRequestException("Student is not in class")
             }
 
+            await tx.answer_for_exam.deleteMany({where: {exam_taken: {et_id}}})
+
+            const updateAnswerData = answers.map((item) => {
+                return item.answers.map((i) => {
+                    return {
+                        ques_id: item.ques_id,
+                        aid: i
+                    }
+                })
+            }).flat()
+
+            await tx.answer_for_exam.createMany({
+                data: updateAnswerData.map((item) => {
+                    return {
+                        ques_id: item.ques_id,
+                        aid: item.aid,
+                        et_id
+                    }
+                })
+            })
+
             const {score, cnt} = await this.calculateScore(tx, answers)
 
-            const takenTime = await tx.exam_taken.create({
+            const now = DateTime.now().setZone('Asia/Ho_Chi_Minh').toJSDate()
+            const examTaken = await tx.exam_taken.update({
+                where: {et_id},
                 data: {
-                    startAt,
-                    doneAt: endAt,
-                    final_score: score,
+                    isDone,
+                    doneAt: now,
                     total_ques_completed: cnt,
-                    exam_id,
-                    session_id,
-                    student: {connect: {uid: student_id}}
+                    final_score: score
                 }
             })
 
             return {
                 message: "Exam Submitted Successfully",
-                data: takenTime
+                data: examTaken
             }
         })  
     }
