dc7732cd2b3ea6e8ed0420f8d86256939273171b
dCyberTech <maidangduong92tn@gmail.com>
2026-03-24 14:59:26 +0700

    tutor dashboard: categories need to be noticed

--- tutor-service/src/dashboard/dashboard.controller.ts
+++ tutor-service/src/dashboard/dashboard.controller.ts
@@ -2,9 +2,12 @@ import {
     Controller,
     Get,
     Param,
-    UseGuards
+    UseGuards,
+    Request,
+    ParseIntPipe,
+    Query
 } from "@nestjs/common";
-import { DashboardService } from "./dashboard.service";
+import { DashboardService, TutorDashboard } from "./dashboard.service";
 import { JwtAuthGuard } from "src/auth/guard/jwt-auth.guard";
 import { RolesGuard } from "src/auth/guard/roles.guard";
 import { Roles } from "src/auth/decorator/roles.decorator";
@@ -12,12 +15,24 @@ import { Roles } from "src/auth/decorator/roles.decorator";
 @Controller("dashboard")
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class DashboardController {
-    constructor(private readonly dashboardService: DashboardService) {}
+    constructor(
+        private readonly dashboardService: DashboardService,
+        private readonly tutorDashboard: TutorDashboard
+    ) {}
 
     @Get('admin-stats')
     @Roles('admin')
     getAdminStats(){
         return this.dashboardService.getAdminStats()
+    }
+        
+    @Get('tutor-stats/exam-session')
+    getExamSessionStats(
+        @Query('day_range', ParseIntPipe) day_range: number,
+        @Request() req
+    ) {
+        const uid = req.user.userId
+        return this.tutorDashboard.getWeeklyClassESProgress(uid, day_range)
     }
 
     @Get('tutor-stats/:tutor_id')
