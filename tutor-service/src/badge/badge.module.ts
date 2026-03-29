0008d84ef9588eb1f855a2f2ee28f3ea6bc8d826
dCyberTech <maidangduong92tn@gmail.com>
2026-03-29 22:39:57 +0700

    badge: create and get all badges

--- tutor-service/src/badge/badge.module.ts
+++ tutor-service/src/badge/badge.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { BadgeService } from "./badge.service";
 import { BadgeController } from "./badge.controller";
+import { CloudinaryService } from "src/resource/cloudinary/cloudinary.service";
 
 @Module({
   imports: [],
   controllers: [BadgeController],
-  providers: [BadgeService],
+  providers: [BadgeService, CloudinaryService],
   exports: [],
 })
 export class BadgeModule{}
--- tutor-service/src/badge/badge.service.ts
+++ tutor-service/src/badge/badge.service.ts
@@ -3,20 +3,24 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { BadgeDto } from "./dto/badge.dto";
 import { ExceptionResponse } from "src/exception/Exception.exception";
 import { AggregateType, BadgeType, ValueType } from "@prisma/client";
+import { CloudinaryService } from "src/resource/cloudinary/cloudinary.service";
 
 @Injectable()
 export class BadgeService {
     constructor(
-        private readonly prisma: PrismaService
+        private readonly prisma: PrismaService,
+        private cloudinary: CloudinaryService
     ){}
 
-    async createBadge(data: BadgeDto){
+    async createBadge(data: BadgeDto, file: Express.Multer.File){
         try {
+            const fileUploader = await this.cloudinary.uploadFile(file)
+
             const newBadge = await this.prisma.badge.create({
                 data: {
                     title: data.title,
                     description: data.description || null,
-                    badge_url: data.badge_url,
+                    badge_url: fileUploader.secure_url,
                     badge_type: data.badge_type as BadgeType,
                     func_type: data.func_type as AggregateType,
                     value_type: data.value_type as ValueType,
@@ -44,5 +48,9 @@ export class BadgeService {
                 
             }
         }
+    }
+
+    async updateBadge() {
+        
     }
 }
