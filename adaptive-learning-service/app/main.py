18b28eedec34bc2abb39f5896280ea1248c72155
dCyberTech <maidangduong92tn@gmail.com>
2026-04-05 09:38:47 +0700

    fastapi: worker async runner

--- adaptive-learning-service/app/main.py
+++ adaptive-learning-service/app/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 from app.api.router import api_router
-
+import asyncio
 from contextlib import asynccontextmanager
-
-from app.core.rabbitmq import rabbitmq
+from app.worker.consumer import start_worker
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-  # startup
-  await rabbitmq.connect()
+  try:
+    # START THE WORKER IN A SEPARATE THREAD
+    worker_task = asyncio.create_task(asyncio.to_thread(start_worker))
+    print("Worker started in background thread")
+  except Exception as e:
+    print("Error starting worker:", e)
 
   yield
 
-  # shutdown
-  await rabbitmq.close()
+  # Note: gracefully shutting down a threaded pika connection 
+  # is a bit trickier, but cancelling the task is a start.
+  worker_task.cancel()
 
 app = FastAPI(title="ML Service", lifespan=lifespan)
 app.include_router(api_router, prefix="/api")
