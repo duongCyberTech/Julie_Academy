6c518d88daad6dd34341b9e13cfa4ef21fe55eb2
dCyberTech <maidangduong92tn@gmail.com>
2026-04-05 09:38:47 +0700

    fastapi: worker async runner

--- adaptive-learning-service/app/worker/consumer.py
+++ adaptive-learning-service/app/worker/consumer.py
@@ -1,3 +1,6 @@
+from time import time
+import traceback
+
 import pika
 import json
 from app.core.config import settings
@@ -8,11 +11,7 @@ instant_computing_service = InstantComputingService()
 def callback(ch, method, properties, body):
   data = json.loads(body)
 
-  print("Received:", data)
-
   result = instant_computing_service.compute_instant_knowledge(data, {}).to_dict()
-
-  print("Processed:", result)
 
   if properties.reply_to:
     response = {
@@ -35,29 +34,38 @@ def callback(ch, method, properties, body):
 
 
 def start_worker():
-  credentials = pika.PlainCredentials(settings.RMQ_USER, settings.RMQ_PASS)
+  while True:
+    try:
+      print("Đang cố gắng kết nối tới RabbitMQ...")
+      credentials = pika.PlainCredentials(settings.RMQ_USER, settings.RMQ_PASS)
+      params = pika.ConnectionParameters(
+        host=settings.RMQ_HOST,
+        port=int(settings.RMQ_PORT), # Đảm bảo port là số nguyên
+        virtual_host=settings.RMQ_VHOST,
+        credentials=credentials
+      )
 
-  params = pika.ConnectionParameters(
-    host=settings.RMQ_HOST,
-    port=settings.RMQ_PORT,
-    virtual_host=settings.RMQ_VHOST,
-    credentials=credentials
-  )
+      connection = pika.BlockingConnection(params)
+      channel = connection.channel()
 
-  connection = pika.BlockingConnection(params)
-  channel = connection.channel()
+      # Lệnh này sẽ thực sự tạo ra queue nếu nó chưa tồn tại
+      channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
 
-  channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
+      channel.basic_qos(prefetch_count=1)
+      channel.basic_consume(
+        queue=settings.RABBITMQ_QUEUE,
+        on_message_callback=callback
+      )
+      print("Worker đã kết nối tới RabbitMQ và đang chờ tin nhắn...")
+      channel.start_consuming()
 
-  channel.basic_qos(prefetch_count=1)
-
-  channel.basic_consume(
-    queue=settings.RABBITMQ_QUEUE,
-    on_message_callback=callback
-  )
-
-  print("Waiting for messages...")
-  channel.start_consuming()
+    except pika.exceptions.AMQPConnectionError:
+      print("RabbitMQ chưa khởi động xong. Sẽ thử lại sau 5 giây...")
+      time.sleep(5)
+    except Exception as e:
+      print("WORKER BỊ LỖI NGHIÊM TRỌNG:")
+      traceback.print_exc()
+      time.sleep(5)
 
 if __name__ == "__main__":
   start_worker()
