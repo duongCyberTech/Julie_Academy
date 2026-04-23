d5dce1ae28a791fb2d73cf24d23540f5edd936d9
dCyberTech <maidangduong92tn@gmail.com>
2026-04-23 15:52:12 +0700

    fix: internal role router

--- tutor-service/src/question/question.controller.ts
+++ tutor-service/src/question/question.controller.ts
@@ -59,7 +59,6 @@ export class BookController {
     }
 
     @Get()
-    @Roles('admin')
     getAllLessonPlan(@Request() req){
         const user = req.user
         return this.bookService.getAllPlans(user.userId, user.role);
